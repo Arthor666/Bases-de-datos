05ea93c19b387a74d6330c5b48bd5dc07bee0523
Samuel Raya Chávez <43630037+samiraya23@users.noreply.github.com>
2019-11-28 14:44:48 -0600

    Agregado los privilegios

--- ingreso.php
+++ ingreso.php
@@ -4,14 +4,27 @@ $usuario=$_GET["usuario"];
 $password=$_GET["pass"];
 $query = "SELECT 'hola',idusuario from usuario WHERE correo='".$usuario."' AND contrasenia='".$password."'";
 $result=pg_query($query) or die('La consulta fallo: ' . pg_last_error());
+
+$idusuario = "SELECT idusuario from usuario WHERE correo='".$usuario."' AND contrasenia='".$password."'";
+$resultId=pg_query($idusuario) or die('La consulta fallo: ' . pg_last_error());
+
 $row = pg_fetch_row($result);
-include 'close.php';
 if($row[0]=='hola'){
 	session_start();
 	$_SESSION["usuario"] = $row[1];
 	$_SESSION['start']    = time();
     $_SESSION['expire']   = $_SESSION['start'] + (60 * 60);
-	header("Location: pagina_principal.php");	
+    
+    $Privilegio="SELECT idprivilegio FROM usuario WHERE idusuario='".$_SESSION['usuario']."'";
+	$resultPrivilegio=pg_query($Privilegio) or die('La consulta fallo: ' . pg_last_error());
+	$linea = pg_fetch_array($resultPrivilegio, null, PGSQL_ASSOC);
+	foreach($linea as $col_value){
+    	if($col_value=='1'){
+			header("Location: pagina_principal.php");	
+		}else if($col_value=='2'){
+			header("Location: Administrador.php");
+		}
+	}
 }else {
 	echo'<script type="text/javascript">
     alert("Contraseña o Correo invalidos");
